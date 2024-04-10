e1ad234ea0084cb9012a121aab3356c8564c2d08
Sumit Suryawanshi <suryawanshisumit2003@gmail.com>
2024-04-10 13:53:36 +0530

    update

--- main.py
+++ main.py
@@ -9,9 +9,10 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 model = genai.GenerativeModel('gemini-pro-vision')
 
-def get_gemini_response(imput,image,prompt):
+def get_gemini_response(input,image,prompt):
     response = model.generate_content([input,image[0],prompt])
-    return response.text
+    # print(response[0])
+    return response
 
 
 def input_image_setup(upload_file):
@@ -31,7 +32,6 @@ st.set_page_config(page_title="Multilanguage Invoice Extractor")
 
 st.header("GEmini Application")
 
-input = st.text_input("Input Prompt: ",key="imput")
 upload_file = st.file_uploader("choose an image of invoice...",type=["jpg","jpeg","png"])
 image=""
 
@@ -39,15 +39,22 @@ if upload_file is not None:
     image = Image.open(upload_file)
     st.image(image,caption="Uploaded Image.",use_column_width=True)
 
-submit = st.button("Tell me about the Invoice")
+
+input = st.text_input("Input Prompt: ",key="imput")
+submit = st.button("Submit Query")
 
 input_prompt = """ 
 You are an expert in understanding invoices. we will upload a image as invoice and
-you will have to answer any questuions based on the uploaded invoice image
+you will have to answer any questions based on the uploaded invoice image
+"""
+
+input_prompt = """ 
+You are an expert in understanding input images. we will upload a image and
+you will have edit image according to input prompt and return edited image in jpg format. use output format {"image":edited image}.
 """
 
 if submit:
     image_data = input_image_setup(upload_file)
     response = get_gemini_response(input_prompt,image_data,input)
     st.subheader("The Response is")
-    st.write(response)
+    st.write(response.text)
